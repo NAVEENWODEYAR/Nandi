9c34dcaf308ea574ddc062745351c98e37acde09
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-11 12:24:05 +0530

    Collectors is a final class that extends the Object class

---
+++ src/main/java/com/nandi/collectors/CollectorsMethods.java
@@ -0,0 +1,11 @@
+package com.nandi.collectors;
+
+public class CollectorsMethods 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("\nCollectors is a final class that extends the Object class.\n");
+	}
+
+}
