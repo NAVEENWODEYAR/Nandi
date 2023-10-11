27f8e0b98e5cc8503604c5b4dcfcf012648b2c3f
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-11 13:27:32 +0530

    It is used to accumulate elements into a set, It will remove all the duplicate entries.

--- src/main/java/com/nandi/collectors/CollectorsMethods.java
+++ src/main/java/com/nandi/collectors/CollectorsMethods.java
@@ -1,12 +1,13 @@
 package com.nandi.collectors;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class CollectorsMethods 
 {
 
-	// toList()
+	//1. toList(),It is used to accumulate elements into a list. It will create a new list (It will not change the current list).
 	static void listFromcolln(List<Integer> list)
 	{
 		System.out.println("\n" +list+" \n");
@@ -14,7 +15,14 @@ public class CollectorsMethods
 			.map(num -> num+10)
 			.collect(Collectors.toList())
 			.forEach(System.out::println);
-		
+	}
+	
+	// 2. toSet(), It is used to accumulate elements into a set, It will remove all the duplicate entries.
+	static void setFromColln(List<Integer> intList)
+	{
+		System.out.println("\n" +intList+" \n");
+		Set<Integer> collect = intList.stream().collect(Collectors.toSet());
+		System.out.println("\n" +collect+" \n");
 	}
 	
 	public static void main(String[] args) 
@@ -23,9 +31,12 @@ public class CollectorsMethods
 		System.out.println("\nIt provides reduction operations, such as accumulating elements into collections, summarizing elements according to various criteria, etc."
 				+ "\n Java Collectors class provides various methods to deal with elements.n");
 		
-		List<Integer> intList = List.of(1,2,3,4,5);
+		List<Integer> intList = List.of(1,2,3,4,5,4,3,2);
 		System.out.println("\n******\n");
 		listFromcolln(intList);
+		System.out.println("\n******\n");
+		setFromColln(intList);
+		System.out.println("\n******\n");
 	}
 
 }
