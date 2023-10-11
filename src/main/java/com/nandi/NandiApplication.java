e8ede50dba16f4edfd6dea7c2341ca53b3c5acc6
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-11 12:04:17 +0530

    c

---
+++ src/main/java/com/nandi/NandiApplication.java
@@ -0,0 +1,16 @@
+package com.nandi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NandiApplication 
+{
+
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(NandiApplication.class, args);
+		System.out.println("nandi gives delight,");
+	}
+
+}
