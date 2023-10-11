2b023d8af9cc23c9670baf2fa0500db17e4ab3f3
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-11 13:20:06 +0530

    It is used to accumulate elements into a list. It will create a new list (It will not change the current list).

--- src/main/java/com/nandi/collectors/CollectorsMethods.java
+++ src/main/java/com/nandi/collectors/CollectorsMethods.java
@@ -1,11 +1,31 @@
 package com.nandi.collectors;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class CollectorsMethods 
 {
 
+	// toList()
+	static void listFromcolln(List<Integer> list)
+	{
+		System.out.println("\n" +list+" \n");
+		list.stream()
+			.map(num -> num+10)
+			.collect(Collectors.toList())
+			.forEach(System.out::println);
+		
+	}
+	
 	public static void main(String[] args) 
 	{
 		System.out.println("\nCollectors is a final class that extends the Object class.\n");
+		System.out.println("\nIt provides reduction operations, such as accumulating elements into collections, summarizing elements according to various criteria, etc."
+				+ "\n Java Collectors class provides various methods to deal with elements.n");
+		
+		List<Integer> intList = List.of(1,2,3,4,5);
+		System.out.println("\n******\n");
+		listFromcolln(intList);
 	}
 
 }
