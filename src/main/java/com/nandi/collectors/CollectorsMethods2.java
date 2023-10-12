29b23c29b0b49281c0f48a47363a33eebcadef0f
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-12 16:51:52 +0530

    averagingInt()

--- src/main/java/com/nandi/collectors/CollectorsMethods2.java
+++ src/main/java/com/nandi/collectors/CollectorsMethods2.java
@@ -7,21 +7,32 @@ public class CollectorsMethods2
 {
 
 	// 1. Averaging long: averagingLong(),Finds the average value of a collection of long values.
-	static void averageInList(List<Integer> list)
+	static void avgLong(List<Integer> list)
 	{
 		System.out.println("\n It will return a double value, not long value,\n");
-//		Double avg = list.stream().collect(Collectors.averagingLong(n-> n*2));
-		Double avg = list.stream().collect(Collectors.averagingLong(null));
-		System.out.println("\nAverage value in the list,"+avg);
+		Double avgLong = list.stream().collect(Collectors.averagingLong(n-> n*2));
+		System.out.println("\nAverage long value in the list,"+avgLong);
 	}
+	
+	//2. Averaging int: averagingInt(),Finds the average value of a collection of integer values.
+	static void avgInt(List<Integer> list)
+	{
+		System.out.println("It will return a Double value, not an int value.");
+		Double avgInt = list.parallelStream().collect(Collectors.averagingInt(num -> num+1));
+		System.out.println("\n Average int value in the list,"+avgInt);
+	}
+	
+	//
 	public static void main(String[] args) 
 	{
 		System.out.println("\nCollectors is a final class that extends the Object class.\n");
 	
 		List<Integer> intList = List.of(1,2,3,4,5);
+		System.out.println(intList);
 		System.out.println("\n******\n");
-		averageInList(intList);
+		avgLong(intList);
 		System.out.println("\n******\n");
+		avgInt(intList);
 		System.out.println("\n******\n");
 		System.out.println("\n******\n");
 	}
