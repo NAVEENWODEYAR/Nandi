bd9a949f8e1dcc55cf75c58bfc5fa60e8cf5adda
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-12 16:13:28 +0530

    ge

--- src/main/java/com/nandi/array/SecLargestInArr.java
+++ src/main/java/com/nandi/array/SecLargestInArr.java
@@ -1,10 +1,28 @@
 package com.nandi.array;
 
-public class SecLargestInArr {
+import java.util.Arrays;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class SecLargestInArr 
+{
+	static void secLargest(int[] names)
+	{
+		int largest = 0;
+		int secLargest = 0;
+		
+		for(int i=0; i<names.length-1; i++)
+		{
+			if()
+		}
+	}
+	public static void main(String[] args)
+	{
+		System.out.println("\n Collection of homogenous elements stored in contiguos memory locations\n");
+		int[] marks = new int[5];
+		for(int i=0; i<5; i++)
+		{
+			marks[i]= i; 
+		}
+		System.out.println(Arrays.toString(marks));
 	}
 
 }
